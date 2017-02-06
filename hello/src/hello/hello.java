e384bffa3032db1ea6ac313235ab81c85dc99124
Anil <Anil@Anil-PC>
2017-02-06 14:13:50 +0530

    hiii make commit

---
+++ hello/src/hello/hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class hello {
+	public static void main(String args[])
+	{
+		System.out.println("hello world");
+		System.out.println("hello chetsa");
+		System.out.println("hello meghsa");
+		System.out.println("hello motisa");
+
+	}
+
+}
